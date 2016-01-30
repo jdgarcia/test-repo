807c290d423640cfd9f027343bd294103a8e73b9
Jose Garcia <joseg@axosoft.com>
2016-01-29 21:36:05 -0700

    "new feature"

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // TODO: The rest of all the things
 
 var HELLO_WORLD_STR = 'Hello world!';
+var NEW_FEATURE = 'I\'m a new feature!';
 
 function main() {
   console.log(HELLO_WORLD_STR);
+  console.log(NEW_FEATURE);
 }
 
 main();
